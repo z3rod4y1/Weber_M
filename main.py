dfb2637f8964ff3e425ec9d005c9f0d922fcbbe1
Binah <60533911+z3rod4y1@users.noreply.github.com>
2025-04-24 13:25:59 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 import tkinter
 import time
 import random
+from tkinter import ttk
 
 
 db = sqlite3.connect("dorogi.db")
@@ -18,12 +19,26 @@ root.config(width=600, height=700)
 
 canva = tkinter.Canvas(root, width=600, height=700, background="green")
 
+a = cur.execute("SELECT * FROM road_controll")
+road_controll = a.fetchall()
+print(road_controll)
+canva.create_rectangle(road_controll[0][1] + 25, road_controll[0][2] + 25, road_controll[0][1] - 25, road_controll[0][2] - 25, fill="lightgreen")
 
+a = cur.execute("SELECT * FROM peshehod")
+peshehod = a.fetchall()
+
+canva.create_oval(peshehod[0][1] - 25, peshehod[0][2] - 25, peshehod[0][1] + 25, peshehod[0][2] + 25, fill = "white")
 canva.create_line(dorogi[0][1], dorogi[0][2], dorogi[0][3], dorogi[0][4], width = 10)
 canva.create_line(dorogi[1][1], dorogi[1][2], dorogi[1][3], dorogi[1][4], width = 10)
 canva.create_line(dorogi[2][1], dorogi[2][2], dorogi[2][3], dorogi[2][4], width = 10)
 rand = random.randint(0, 1)
 canva.pack()
+
+cnopka = ttk.Button(text="Переключение режима")
+cnopka.pack()
+
+start_stop = ttk.Button(text="Старт и стоп обучения")
+start_stop.pack()
 
 def move(dorogi):
     roads = dorogi
@@ -39,6 +54,7 @@ def move(dorogi):
     cross = 0
     cross_go = random.randint(0, 1)
     up = 0
+
     while end != 1:
         cords = canva.coords(car)
 
@@ -46,6 +62,7 @@ def move(dorogi):
             kx, ky = 0, 1
         elif cords[0] + 25 == 400 and -1 < cords[1] + 25 < 500 and up == 0:
             kx, ky = 0, 1
+
         elif cords[1] + 25 == 500 and -1 < cords[0] + 25 < 600 and rand == 0:
             if cords[0] + 25 == 300 and -1 < cords[1] + 25 < 501 and cross_go == 1:
                 kx, ky = 0, -1
@@ -55,7 +72,6 @@ def move(dorogi):
                 up = 1
             else:
                 kx, ky = 1, 0
-
         elif cords[1] + 25 == 500 and -1 < cords[0] + 25 < 601 and rand == 1:
             if cords[0] + 25 == 300 and -1 < cords[1] + 25 < 501 and cross_go == 1:
                 kx, ky = 0, -1
